d6b414f25b4850a0519df1dc52d40082b3d5e0ae
Dripmaster <son342@gmail.com>
2020-01-25 16:13:50 +0900

    First Commit

---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour
+{
+    public float speed = 1.0f;
+    Vector2 moveDir = Vector2.zero;
+    bool slideChance = true;
+    void Update()
+    {
+        if (slideChance == true && Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                moveDir = new Vector2(transform.position.x, transform.position.y + Input.GetTouch(0).deltaPosition.y * speed);
+            }
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                slideChance = false;
+            }
+        }
+        else if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved)) {
+            moveDir.y *= 0.995f;
+        }
+         if(moveDir.y>transform.position.y)
+         transform.position = Vector2.Lerp(transform.position, moveDir, Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/cameraRig.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraRig : MonoBehaviour
+{
+    public Transform iceTransform;
+    public float cameraSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        transform.position = iceTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector2.Lerp(transform.position,iceTransform.position,Time.deltaTime*cameraSpeed);
+    }
+}
